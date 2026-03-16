af4cc0bda0289b233b9471af167600b63df2ae56
mi-ko-san <m.snow.2200@gmail.com>
2026-03-17 00:05:29 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+var CACHE_NAME = 'auto-trade-v1';
+var FILES = [
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(FILES);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    fetch(e.request).catch(function() {
+      return caches.match(e.request);
+    })
+  );
+});
